059679e51e49f2e1a80896fcd22b3cff081c5683
tasnime <tasnim.fr2008@gmail.com>
2024-06-05 03:56:32 -0700

    task

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -8,7 +8,7 @@
  * Return: a pointer to the root node of the created AVL tree
  *         NULL on failure
  */
-avl_t *array_to_avl(int *array, size_t size)
+avl_t *array_to_avl(int *array, size_t size);
 {
 	unsigned int i;
 	avl_t *root = NULL;
